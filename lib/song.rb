ac6f4f13e4cc0d93dd5f2098cddd4b39aff3656b
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-10-15 23:45:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 #Songs should have a title and belong to an artist. A song should be able to tell you the name of its artist:
 
 class Song 
-  attr_accessor :title Songs should have a title and belong to an artist. A song should be able to tell you the name of its artist:
+  attr_accessor :title, :artist
   
 end
